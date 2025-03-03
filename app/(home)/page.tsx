381428814eecb8bffdf63007ec84a7428085e706
Luciano Oliveira <aulascursos66@gmail.com>
2025-03-03 11:40:27 -0300

    feat: add qualidade component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,18 @@
+import Hero from "./_components/hero";
+import Qualidades from "./_components/qualidades";
 
 const  HomePage = () => {
-  return ( <h1 className="bg-500">Home</h1> );
+  return ( 
+    <>
+      <h1 className="bg-500">Home</h1> 
+      <div className="w-full items-center justify-center space-y-6">
+        <Hero/>
+        <div >
+          <Qualidades/>
+        </div>
+      </div>
+    </>
+);
 }
 
 export default HomePage
