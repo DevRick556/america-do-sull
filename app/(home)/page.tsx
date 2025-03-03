b681dcdcc88b3ee53e70bb399742efeed7c06d38
Rickelme Pereira de Queiroz <rickpereiradequeiroz@gmail.com>
2025-03-03 11:41:38 -0400

    feat: add responsive header component and menu hamburg

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,6 @@
-import Hero from "./_components/hero";
-import Qualidades from "./_components/qualidades";
 
 const  HomePage = () => {
-  return ( 
-    <>
-      <h1 className="bg-500">Home</h1> 
-      <div className="w-full items-center justify-center space-y-6">
-        <Hero/>
-        <div >
-          <Qualidades/>
-        </div>
-      </div>
-    </>
-);
+  return ( <h1 className="bg-500">Home</h1> );
 }
 
 export default HomePage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import Header from "./_components/header";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,7 +23,10 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <Header />
         {children}
+        
+        
       </body>
     </html>
   );
