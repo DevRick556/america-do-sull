e2efc6324b84675fb760a1b744546571a088918a
Luciano Oliveira <aulascursos66@gmail.com>
2025-03-03 16:53:05 -0300

    feat: add new sessions to homePage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,20 @@
+import Hero from "./_components/hero";
+import Qualidades from "./_components/qualidades";
+import Servicos from "./_components/servicos";
 
 const  HomePage = () => {
-  return ( <h1 className="bg-500">Home</h1> );
+  return ( 
+    <div className="w-full items-center justify-center space-y-6">
+      <Hero/>
+      <div>
+        <Qualidades/>
+      </div>
+      <div className="p-4 space-y-3">
+      <h2>Serviços</h2>
+        <Servicos />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage
